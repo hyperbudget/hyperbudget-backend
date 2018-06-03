d88968057a0f7ad2223f140e044283978c13ebae
Errietta Kostala <errietta@errietta.me>
2018-06-03 10:38:04 +0100

    Add user model

---
+++ src/lib/schema/user.ts
@@ -0,0 +1,22 @@
+import { Document, Schema, Model, model } from 'mongoose';
+
+export type User = {
+  email: string;
+  firstName: string,
+  password: string,
+};
+
+export interface IUserModel extends User, Document {}
+
+export const UserSchema: Schema = new Schema({
+  email: String,
+  password: String,
+  firstName: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastLogin: Date,
+});
+
+export const UserModel: Model<IUserModel> = model<IUserModel>("user", UserSchema);
--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 import { check, validationResult } from 'express-validator/check';
 
+import { UserModel, User } from '../lib/schema/user';
+
 export const validateRegistration = [
   check('email').isEmail(),
   check('password').isLength({ min: 8 }),
+  check('firstname').isAlphanumeric(),
 ];
 
 export const register = (req: Request, res: Response) => {
@@ -13,8 +16,18 @@ export const register = (req: Request, res: Response) => {
     return res.status(422).json({ error: errors.array() });
   }
 
-  let { email, password } = req.body;
+  let { email, password, firstname } = req.body;
   console.log(email);
+
+  UserModel.create({
+    email: email,
+    password: password,
+    firstName: firstname,
+  }).then(() => UserModel.findOne({
+    email: email
+  })).then((user: User) => {
+    console.log("found user", user);
+  });
 
   res.json({
     success: 1,
