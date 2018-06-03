ac8262c423d06173bb9d132fb07890ef83726a8a
Errietta Kostala <errietta@errietta.me>
2018-06-03 19:46:58 +0100

    add auth middleware `

---
+++ src/lib/middleware/authmiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { SystemConfig } from '../config/system';
+
+export const authMiddleware = (req: Request, res: Response, next) => {
+  jwt.verify(
+    req.get('x-jwt'),
+    SystemConfig.config.app.token_secret,
+    (err, decoded) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).json({
+          errors: [{
+            msg: 'not authenticated',
+          }]
+        });
+      }
+
+      next();
+    }
+  );
+}
