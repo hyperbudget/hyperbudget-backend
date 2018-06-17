b5ce81e7244ced37b9cbc217c393c9a3d94a8748
Errietta Kostala <errietta@errietta.me>
2018-06-17 19:27:33 +0100

    add crypto lib

---
+++ t/crypto.spec.ts
@@ -0,0 +1,33 @@
+import { encrypt, decrypt } from '../src/lib/crypto';
+
+import { expect, assert } from 'chai';
+import 'mocha';
+
+describe('Crypto', () => {
+  it('Encrypts & Decrypts', () => {
+    return encrypt('test', ['passpass']).then((encrypted) => {
+      expect(encrypted).to.match(/^-----BEGIN PGP MESSAGE-----/);
+      expect(encrypted).to.be.a('string');
+      return encrypted;
+    }).then((encrypted) => {
+      return decrypt((encrypted), ['passpass']).then((decrypted) => {
+        expect(decrypted).to.equal('test');
+      });
+    });
+  });
+  it('Errors if it cannot decrypt', () => {
+    return encrypt('test', ['passpass']).then((encrypted) => {
+      expect(encrypted).to.match(/^-----BEGIN PGP MESSAGE-----/);
+      expect(encrypted).to.be.a('string');
+      return encrypted;
+    }).then((encrypted) => {
+      return decrypt((encrypted), ['wrong password']).then(
+        () => {
+        },
+        (err) => {
+          expect(err).to.match(/Session key decryption failed/);
+        }
+      );
+    });
+  });
+});
